7a745d40fd3309cc5e941abb2df6008ec91f6aaf
ezra.fa <ezra.fa@centraltest.com>
2024-09-20 12:03:50 +0200

    fix netlify functions result

--- netlify/functions/config.js
+++ netlify/functions/config.js
@@ -1,11 +1,28 @@
+// exports.handler = async (event) => {
+//     const response = {
+//         statusCode: 200,
+//         body: JSON.stringify({
+//             airtableApiKey: process.env.AIRTABLE_API_KEY,
+//             baseId: process.env.BASE_ID,
+//             baseUrl: process.env.BASE_URL,
+//             tableName: process.env.TABLE_NAME
+//         })
+//     };
+//     return response;
+// };
+
 exports.handler = async (event) => {
+    const airtableApiKey = process.env.AIRTABLE_API_KEY; // Don't return this to the client
+    const baseId = process.env.BASE_ID;
+    const baseUrl = process.env.BASE_URL;
+    const tableName = process.env.TABLE_NAME;
+
     const response = {
         statusCode: 200,
         body: JSON.stringify({
-            airtableApiKey: process.env.AIRTABLE_API_KEY,
-            baseId: process.env.BASE_ID,
-            baseUrl: process.env.BASE_URL,
-            tableName: process.env.TABLE_NAME
+            baseId,
+            baseUrl,
+            tableName
         })
     };
     return response;
